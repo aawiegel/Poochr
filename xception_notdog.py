e13af115ed81ef4be1ee1fa8ea441f6e1b6556f5
Aaron W <aawiegel@gmail.com>
2017-09-06 21:22:46 -0700

    train script

---
+++ xception_notdog.py
@@ -0,0 +1,72 @@
+
+# coding: utf-8
+
+from keras.models import Model
+from keras.optimizers import SGD
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras.layers.pooling import GlobalAveragePooling2D
+from keras.preprocessing.image import ImageDataGenerator
+from keras.applications.xception import Xception
+
+batch_size = 10
+
+# this is the augmentation configuration we will use for training
+train_datagen = ImageDataGenerator(rescale=1./255)
+
+# this is the augmentation configuration we will use for testing:
+# only rescaling
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+# this is a generator that will read pictures found in
+# subfolers of 'data/train', and indefinitely generate
+# batches of augmented image data
+train_generator = train_datagen.flow_from_directory(
+        'data/Images/training/',  # this is the target directory
+        target_size=(299, 299),  # all images will be resized to 224x224
+        batch_size=batch_size,
+        class_mode='binary')  # since we use binary_crossentropy loss, we need binary labels
+
+# this is a similar generator, for validation data
+validation_generator = test_datagen.flow_from_directory(
+        'data/Images/test/',
+        target_size=(299, 299),
+        batch_size=batch_size,
+        class_mode='binary')
+
+
+base_model = Xception(weights='imagenet', include_top=False, input_shape=(299, 299, 3))
+
+
+# Freeze convolutional layers
+for layer in base_model.layers[:-3]:
+    layer.trainable = False
+
+
+
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+x = Dense(1000)(x)
+x = Dense(1)(x)
+x = Activation('sigmoid')(x)
+
+
+model = Model(inputs=base_model.input, outputs=x)
+
+model.compile(optimizer=SGD(lr=0.0001, momentum=0.9),
+            loss='binary_crossentropy', metrics=['accuracy'])
+
+
+model.fit_generator(
+        train_generator,
+        steps_per_epoch=35850 // batch_size,
+        epochs=5,
+        validation_data=validation_generator,
+        validation_steps=15367 // batch_size)
+model.save_weights('xception_backalayer_notdog.h5')
+
+
+
+
+
