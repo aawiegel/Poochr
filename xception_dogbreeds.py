7b85c0bcab5f36700da1bfa5d7a1d74898ef12d9
Aaron W <aawiegel@gmail.com>
2017-09-08 11:53:28 -0700

    class weights

--- xception_dogbreeds.py
+++ xception_dogbreeds.py
@@ -9,6 +9,8 @@ from keras.layers import Activation, Dropout, Flatten, Dense
 from keras.layers.pooling import GlobalAveragePooling2D
 from keras.preprocessing.image import ImageDataGenerator
 from keras.applications.xception import Xception
+from sklearn.utils import class_weight
+import numpy as np
 
 batch_size = 10
 
@@ -42,6 +44,10 @@ validation_generator = test_datagen.flow_from_directory(
         batch_size=batch_size,
         class_mode='categorical')
 
+# Find the class weights since not_dog is overrepresented
+class_weight = class_weight.compute_class_weight('balanced', np.unique(train_generator.classes),train_generator.classes)
+
+class_weights = dict(zip(dict(validation_generator.class_indices).values(), class_weight))
 
 base_model = Xception(include_top=False, input_shape=(299, 299, 3))
 
@@ -69,12 +75,12 @@ model.compile(optimizer=SGD(lr=0.0001, momentum=0.9),
 
 model.fit_generator(
         train_generator,
-        steps_per_epoch=32745 // batch_size,
-        epochs=5,
-        verbose=2,
+        steps_per_epoch=(29682 * 1.2) // batch_size,
+        epochs=2,
+        class_weight=class_weights,
         validation_data=validation_generator,
-        validation_steps=17374 // batch_size)
-model.save_weights('xception_breeds_multiclass_fixed_conv.h5')
+        validation_steps=20437 // batch_size)
+model.save_weights('xception_breeds_weighted.h5')
 
 
 
