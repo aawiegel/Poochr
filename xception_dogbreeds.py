69c3c1a3c1fe0a153f6d46843b7bd165d0a27f12
AaronW <aawiegel@gmail.com>
2017-09-12 23:15:24 +0000

    deep learning with pitbulls

--- xception_dogbreeds.py
+++ xception_dogbreeds.py
@@ -53,7 +53,7 @@ base_model = Xception(include_top=False, input_shape=(299, 299, 3))
 
 
 # Freeze convolutional layers
-for layer in base_model.layers:
+for layer in base_model.layers[:-3]:
     layer.trainable = False
 
 
@@ -68,7 +68,7 @@ x = Activation('softmax')(x)
 
 model = Model(inputs=base_model.input, outputs=x)
 
-model.load_weights('xception_breeds_wgt_reg_conv2.h5', by_name=True)
+model.load_weights('xception_breeds_wgt_reg_conv3.h5', by_name=True)
 
 model.compile(optimizer=SGD(lr=0.0001, momentum=0.9),
             loss='categorical_crossentropy', metrics=['accuracy'])
@@ -77,11 +77,11 @@ model.compile(optimizer=SGD(lr=0.0001, momentum=0.9),
 model.fit_generator(
         train_generator,
         steps_per_epoch=(29682 * 1.2) // batch_size,
-        epochs=2,
+        epochs=5,
         class_weight=class_weights,
         validation_data=validation_generator,
         validation_steps=20437 // batch_size)
-model.save_weights('xception_breeds_wgt_reg_conv3.h5')
+model.save_weights('xception_breeds_wgt_reg_conv4.h5')
 
 
 
