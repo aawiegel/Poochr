a97044ab21b4505cf92fc73f0ee7e4997e2c5be4
AaronW <aawiegel@gmail.com>
2017-09-13 19:11:29 +0000

    save class indices

--- xception_dogbreeds.py
+++ xception_dogbreeds.py
@@ -44,6 +44,10 @@ validation_generator = test_datagen.flow_from_directory(
         batch_size=batch_size,
         class_mode='categorical')
 
+# Save category indices
+
+np.save('breed_indices.npy', validation_generator.class_indices)
+
 # Find the class weights since not_dog is overrepresented
 class_weight = class_weight.compute_class_weight('balanced', np.unique(train_generator.classes),train_generator.classes)
 
