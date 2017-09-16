a5825d8b034c39742739545ff173b5254a52048a
Aaron W <aawiegel@gmail.com>
2017-09-15 17:33:30 -0700

    predictions

--- flask/flask_demo.py
+++ flask/flask_demo.py
@@ -5,12 +5,33 @@ Created on Wed Sep 13 16:04:15 2017
 
 @author: aaron
 """
+import os
+
+import numpy as np
 
 from flask import Flask, render_template, request, redirect, url_for
 from flask import send_from_directory, flash
 from flask_bootstrap import Bootstrap
 from werkzeug.utils import secure_filename
-import os
+
+import PIL
+
+from keras.models import load_model
+from keras.preprocessing import image
+from keras.applications.xception import preprocess_input
+from keras import backend as K
+from sklearn.metrics.pairwise import cosine_similarity
+
+# Model files
+
+model = load_model('../xbreeds_model.h5')
+breed_to_index = np.load('../breed_indices.npy').tolist()
+index_to_breed = inv_map = {v: k for k, v in breed_to_index.items()}
+max_breed_mat = np.load('../breed_max_matrix.npy')
+
+generate_dog_features = K.function([model.layers[0].input, K.learning_phase()],
+                                  [model.layers[-2].output])
+
 
 app_root = os.path.dirname(os.path.abspath(__file__))
 
@@ -18,7 +39,7 @@ upload_dir = os.path.join(app_root, 'static', 'uploads')
 
 allowed_ext = set(['jpg', 'jpeg', 'png', 'gif'])
 
-
+imgsize = 500, 500
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -44,7 +65,8 @@ def upload_file():
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            dog_file = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(dog_file)
             return redirect(url_for('predict_file',
                                     filename=filename))
     return render_template('index_orig.html')
@@ -52,7 +74,19 @@ def upload_file():
 
 @app.route('/predict/<filename>')
 def predict_file(filename):
-    return render_template('picture_show.html', filename=filename)
+    dog_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    img = image.load_img(dog_path, target_size=(299,299))
+    img = image.img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    img = preprocess_input(img)
+    dog_vec = generate_dog_features([img, 0])
+    best_guess = np.argmax(dog_vec)
+    if best_guess == 114:
+        message = "Are you sure this is a dog?"
+    else:
+        message = "This looks like it may be a "+\
+            index_to_breed[best_guess].split("-", 1)[1]
+    return render_template('picture_show.html', filename=filename, message=message)
 
 @app.route('/uploads/<filename>')
 def send_file(filename):
