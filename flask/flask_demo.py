fda794079b507cd591ff90bb4125a42bc66ed86e
Aaron W <aawiegel@gmail.com>
2017-09-18 13:56:16 -0700

    text+image recommendations

--- flask/flask_demo.py
+++ flask/flask_demo.py
@@ -14,23 +14,31 @@ from flask import send_from_directory, flash
 from flask_bootstrap import Bootstrap
 from werkzeug.utils import secure_filename
 
-import PIL
+import pickle
+
+from scipy.sparse import load_npz
 
 from keras.models import load_model
 from keras.preprocessing import image
 from keras.applications.xception import preprocess_input
 from keras import backend as K
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 # Model files
 
 model = load_model('../xbreeds_model.h5')
 breed_to_index = np.load('../breed_indices.npy').tolist()
-index_to_breed = inv_map = {v: k for k, v in breed_to_index.items()}
+index_to_breed = {v: k for k, v in breed_to_index.items()}
 max_breed_mat = np.load('../breed_max_matrix.npy')
+with open('../dog_vocabulary.p', 'rb') as file:
+    dog_vocab = pickle.load(file)
+dog_tfidf_mat = load_npz('../dog_vocab_matrix.npz')
 
 generate_dog_features = K.function([model.layers[0].input, K.learning_phase()],
                                   [model.layers[-2].output])
+
+dog_tfidf = TfidfVectorizer(vocabulary=dog_vocab, ngram_range = (1,2))
 
 
 app_root = os.path.dirname(os.path.abspath(__file__))
@@ -54,6 +62,8 @@ def allowed_file(filename):
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
+        desc = request.form['desc']
+        
         if 'image' not in request.files:
             flash('No file part')
             return redirect(request.url)
@@ -67,6 +77,9 @@ def upload_file():
             filename = secure_filename(file.filename)
             dog_file = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(dog_file)
+            dog_txt_file = dog_file.split('.', 1)[0]+'.txt'
+            with open(dog_txt_file, 'w') as file:
+                file.write(desc)
             return redirect(url_for('predict_file',
                                     filename=filename))
     return render_template('index_orig.html')
@@ -74,7 +87,15 @@ def upload_file():
 
 @app.route('/predict/<filename>')
 def predict_file(filename):
+    
     dog_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    dog_txt_file = dog_path.split('.', 1)[0]+'.txt'
+    
+    with open(dog_txt_file, 'r') as file:
+        desc = file.read()
+    
+    message = desc
+    
     img = image.load_img(dog_path, target_size=(299,299))
     img = image.img_to_array(img)
     img = np.expand_dims(img, axis=0)
@@ -82,10 +103,24 @@ def predict_file(filename):
     dog_vec = generate_dog_features([img, 0])
     best_guess = np.argmax(dog_vec)
     if best_guess == 114:
-        message = "Are you sure this is a dog?"
-    else:
-        message = "This looks like it may be a "+\
-            index_to_breed[best_guess].split("-", 1)[1]
+        message += "Are you sure this is a dog?<br/>"
+
+    dog_text_vec = dog_tfidf.fit_transform([desc])        
+
+    image_similarity = cosine_similarity(max_breed_mat, dog_vec[0])
+    text_similarity = cosine_similarity(dog_tfidf_mat, dog_text_vec)
+    guess = np.argmax(image_similarity.T)
+    message += "The closest dog based on the image is "+\
+            index_to_breed[guess].split("-", 1)[1]
+    guess = np.argmax(text_similarity.T)
+    message += "<br>The closest dog based on the text is "+\
+            index_to_breed[guess].split("-", 1)[1]
+    weight = 0.5
+    combined_sim = weight*image_similarity + (1-weight)*text_similarity
+    guess = np.argmax(combined_sim.T)
+    message += "<br>The closest dog based on both is "+\
+            index_to_breed[guess].split("-", 1)[1]
+    
     return render_template('picture_show.html', filename=filename, message=message)
 
 @app.route('/uploads/<filename>')
