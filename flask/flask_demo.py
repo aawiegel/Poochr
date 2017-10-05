d149796e2e1bd551eff25d19a36218766bfef3a6
Aaron W <aawiegel@gmail.com>
2017-10-05 16:10:35 -0700

    moved files

--- flask/flask_demo.py
+++ flask/flask_demo.py
@@ -25,15 +25,15 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 # Model files
 
-model = load_model('../xbreeds_model.h5')
-breed_to_index = np.load('../breed_indices.npy').tolist()
+model = load_model('xbreeds_model.h5')
+breed_to_index = np.load('breed_indices.npy').tolist()
 index_to_breed = {v: k for k, v in breed_to_index.items()}
-max_breed_mat = np.load('../breed_max_matrix.npy')
-with open('../dog_vocabulary.p', 'rb') as file:
+max_breed_mat = np.load('breed_max_matrix.npy')
+with open('dog_vocabulary.p', 'rb') as file:
     dog_vocab = pickle.load(file)
-with open('../dog_lsa_matrix.npy', 'rb') as file:
+with open('dog_lsa_matrix.npy', 'rb') as file:
     dog_lsa_mat = np.load(file)
-with open('../dogtime_urls.p', 'rb') as file:
+with open('dogtime_urls.p', 'rb') as file:
     dogtime_url_dict = pickle.load(file)
 
 generate_dog_features = K.function([model.layers[0].input, K.learning_phase()],
@@ -41,7 +41,7 @@ generate_dog_features = K.function([model.layers[0].input, K.learning_phase()],
 
 dog_tfidf = TfidfVectorizer(vocabulary=dog_vocab, ngram_range = (1,2))
 
-with open('../breed_lsa.p', 'rb') as file:
+with open('breed_lsa.p', 'rb') as file:
     breed_lsa = pickle.load(file)
 
 app_root = os.path.dirname(os.path.abspath(__file__))
@@ -68,11 +68,11 @@ def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
         desc = request.form['desc']
-        
+
         if len(desc) > 10000:
             flash('Shorten your text description to less than 10000 characters.')
             return redirect(request.url)
-        
+
         if 'image' not in request.files:
             flash('No file part')
             return redirect(request.url)
@@ -96,15 +96,15 @@ def upload_file():
 
 @app.route('/predict/<filename>')
 def predict_file(filename):
-    
+
     dog_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
     dog_txt_file = dog_path.split('.', 1)[0]+'.txt'
-    
+
     with open(dog_txt_file, 'r') as file:
         desc = file.read()
-    
+
     messages = [desc]
-    
+
     img = image.load_img(dog_path, target_size=(299,299))
     img = image.img_to_array(img)
     img = np.expand_dims(img, axis=0)
@@ -112,12 +112,12 @@ def predict_file(filename):
     dog_vec = generate_dog_features([img, 0])
     best_guess = np.argmax(dog_vec)
     if best_guess == 114:
-        messages.append("""Are you sure this is a dog? 
+        messages.append("""Are you sure this is a dog?
                         Well, I'll give you recommendations, but I doubt they'll be
                         good.""")
 
     dog_text_vec = dog_tfidf.fit_transform([desc])
-    dog_lsa_vec = breed_lsa.transform(dog_text_vec)        
+    dog_lsa_vec = breed_lsa.transform(dog_text_vec)
 
     image_similarity = cosine_similarity(max_breed_mat, dog_vec[0])
     text_similarity = cosine_similarity(dog_lsa_mat, dog_lsa_vec)
@@ -130,18 +130,18 @@ def predict_file(filename):
     weight = 0.5
     combined_sim = weight*image_similarity + (1-weight)*text_similarity
     guesses = np.argsort(combined_sim.T)[0][::-1]
-    
+
     with open(dog_txt_file, 'a') as file:
         file.write("Best guess")
         file.write(str(best_guess))
-        
+
     labels = [index_to_breed[guess].split("-", 1)[0] for guess in guesses[:3]]
     breeds = [index_to_breed[guess].split("-", 1)[1] for guess in guesses[:3]]
     urls = [dogtime_url_dict[breed.lower()] for breed in breeds]
     breeds = [url.split("/")[-1].replace("-", " ").replace("_", " ").title() \
                   for url in urls]
     labels_breeds_urls = zip(labels, breeds, urls)
-    return render_template('predict_image.html', 
+    return render_template('predict_image.html',
                            filename=filename, messages=messages,
                            labels_breeds_urls = labels_breeds_urls)
 
